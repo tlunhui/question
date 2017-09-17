34839436420f00ae5629850dff953a89d695a602
tlunhui <lun_hui@outlook.com>
2017-09-18 07:39:42 +0800

    ssm 搭建

---
+++ src/main/java/org/thethreepig/question/controller/Question.java
@@ -0,0 +1,11 @@
+package org.thethreepig.question.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhaod on 2017/9/17.
+ */
+@Controller
+public class Question {
+
+}
