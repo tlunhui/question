851218411ccc7a7fca227e0ad5de4a0f12c88641
tlunhui <lun_hui@outlook.com>
2017-09-19 07:22:09 +0800

    继续搭框架

--- question.sql
+++ question.sql
@@ -5,33 +5,42 @@ use question;
 
 drop table if EXISTS `users`;
 create table `users`(
-`id` int(10) PRIMARY KEY comment=`id 主键`,
-`name` varchar(20) comment=`用户名`,
-`password` varchar(20) comment=`密码`,
-`last_login_time` TIMESTAMP comment=`最后登录时间`,
-`error_count` int(2) DEFAULT 0 comment=`错误次数`,
-`last_error_time` TIMESTAMP comment=`最后密码错误时间`,
-`deleteed` tinyint(1) DEFAULT 0 comment=`删除标识`,
-`last_prompt_time` TIMESTAMP COMMENT=`上次提示时间`
-)comment=`用户表`;
+`id` int(10) PRIMARY KEY comment 'id 主键',
+`name` varchar(20) comment '用户名',
+`password` varchar(20) comment '密码',
+`last_login_time` TIMESTAMP comment '最后登录时间',
+`error_count` int(2) DEFAULT 0 comment '错误次数',
+`last_error_time` TIMESTAMP comment '最后密码错误时间',
+`deleteed` tinyint(1) DEFAULT 0 comment '删除标识',
+`last_prompt_time` TIMESTAMP COMMENT '上次提示时间'
+)comment '用户表';
 
 drop table if EXISTS `posts`;
 create table `posts`(
-`id` int(20) PRIMARY KEY comment=`id 主键`,
-`title` varchar(100) comment=`标题`,
-`users_id` int(10) COMMENT=`创建人`,
-`content` BLOB COMMENT=`内容`,
-`question_time` TIMESTAMP COMMENT=`发帖时间`,
-`deleteed` tinyint(1) DEFAULT 0 `删除标识`
-) COMMENT=`提问帖子表`;
+`id` int(20) PRIMARY KEY comment 'id 主键',
+`title` varchar(100) comment '标题',
+`users_id` int(10) COMMENT '创建人',
+`type_id` int(20) comment'类型编号',
+`content` BLOB COMMENT '内容',
+`question_time` TIMESTAMP COMMENT '发帖时间',
+`deleteed` tinyint(1) DEFAULT 0 comment '删除标识'
+) COMMENT '提问帖子表';
 
 drop table if EXISTS `answers`;
 create table `answers`(
-`id` int (20) PRIMARY KEY COMMENT=`id 主键`,
-`users_id` int(10) COMMENT=`回答人`,
-`content` blob COMMENT=`回答内容`,
-`time` timestamp COMMENT=`回答时间`,
-`support` int(6) comment=`支持数`,
-`against` int(6) comment=`反对数`,
-`deleteed` tinyint(1) DEFAULT 0 COMMENT=`删除标识`
-)comment=`回答表`;
+`id` int (20) PRIMARY KEY COMMENT 'id 主键',
+`users_id` int(10) COMMENT '回答人',
+`content` blob COMMENT '回答内容',
+`time` timestamp COMMENT '回答时间',
+`support` int(6) comment '支持数',
+`against` int(6) comment '反对数',
+`deleteed` tinyint(1) DEFAULT 0 COMMENT '删除标识'
+)comment '回答表';
+
+drop table if exists `type`;
+create table `type`(
+`id` int (20) primary key comment 'id 主键',
+`type_name` varchar(50) comment '类型名',
+`time` timestamp comment '类型创建时间',
+`deleteed` tinyint(1) comment '删除标识'
+)comment'问题类型表';
